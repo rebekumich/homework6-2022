7cc1de6813ea5aedd1a009a3ff275e8f5deb1688
rebekumich <90056752+rebekumich@users.noreply.github.com>
2022-04-06 14:23:28 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -2,15 +2,17 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video = document.querySelector("#player1")
 	video.autoplay = false;
-	video.loop = false;
+	video.loop = false
 
 });
 
+
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	video.play();
-	document.querySelector("#volume").innerHTML = video.volume * 100 +"%";
+ 	video.play();
+ 	document.querySelector("#volume").innerHTML = video.volume * 100 +"%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
